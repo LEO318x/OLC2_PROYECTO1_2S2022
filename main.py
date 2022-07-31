9f3bee173d7399fec692da04bae12f10525ba9fe
Mike Molina <leo318x@gmail.com>
2022-07-30 22:58:46 -0600

    Interfaz Gráfica

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from tkinter import Tk, Menu, messagebox
+import tkinter
+from tkinter import Tk, Frame, Menu, messagebox, Text, Button, Label, filedialog
 
 class Ventana:
     def __init__(self, master=None):
@@ -18,19 +19,73 @@ class Ventana:
         self.center_y = int(self.screen_height / 2 - self.window_height / 2)
         self.master.geometry(f'{self.window_width}x{self.window_height}+{self.center_x}+{self.center_y}')
 
-        #Menú
+        # Frame Analisis
+        self.frmAnalisis = Frame(self.master)
+        self.frmAnalisis.config(bg='#b6bab1')
+        self.frmAnalisis.grid(row=0, column=0)
+        self.frmAnalisis.rowconfigure(0, minsize=self.window_height, weight=1)
+
+        self.txtEntrada = Text(self.frmAnalisis, width=72)
+        self.txtEntrada.grid(row=0, column=1, padx=10, pady=5, sticky='NSWE')
+
+        self.btnAbrir = Button(self.frmAnalisis, text='Abrir', width=10, command=self.fncAbrirArchivo)
+        self.btnAbrir.grid(row=0, column=2, pady=20, sticky='N')
+
+        self.btnAnalizar = Button(self.frmAnalisis, text='Analizar', width=10, command=self.fncAnalizar)
+        self.btnAnalizar.grid(row=0, column=2, pady=80, sticky='N')
+
+        self.txtSalida = Text(self.frmAnalisis, width=72)
+        self.txtSalida.grid(row=0, column=3, padx=10, pady=5, sticky='NSWE')
+
+        # Menú
         menu = Menu(self.master)
         self.master.config(menu=menu)
 
+        editorMenu = Menu(menu, tearoff=0)
+        editorMenu.add_command(label='Analisis', command=self.analisis_gui)
+        menu.add_cascade(label='Editor', menu=editorMenu)
+
+        reportesMenu = Menu(menu, tearoff=0)
+        reportesMenu.add_command(label='Reporte de simbolos', command='')
+        reportesMenu.add_command(label='Reporte de errores', command='')
+        reportesMenu.add_command(label='Reporte de base de datos', command='')
+        reportesMenu.add_command(label='Reporte de tablas de base de datos', command='')
+        menu.add_cascade(label='Reportes', menu=reportesMenu)
+
         ayudaMenu = Menu(menu, tearoff=0)
-        ayudaMenu.add_command(label='Acerca de', command=self.acercade)
+        ayudaMenu.add_command(label='Acerca de', command=self.acerca_de)
         ayudaMenu.add_command(label='Salir', command=self.salir)
         menu.add_cascade(label='Ayuda', menu=ayudaMenu)
 
-    def acercade(self):
+    def analisis_gui(self, master=None):
+        self.frmAnalisis.grid()  # Mostramos el frame analisis
+
+    def fncAbrirArchivo(self):
+        filetypes = (
+            ('text files', '*.txt'),
+            ('All files', '*.*')
+        )
+        # Mostramos la ventana para seleccionar el archivo
+        f = filedialog.askopenfile(filetypes=filetypes)
+        if f:
+            # Limpiamos nuestra area de texto correspondiente
+            self.txtEntrada.delete(1.0, tkinter.END)
+            # Leemos el archivo y ponemos el contenido en el area de texto correspondiente
+            self.txtEntrada.insert('1.0', f.readlines())
+        else:
+            messagebox.showinfo(message="Por favor selecciona un archivo...", title="Información")
+
+    def fncAnalizar(self):
+        self.txtSalida.delete(1.0, tkinter.END)
+        txt = self.txtEntrada.get('1.0', 'end-1c')
+        self.txtSalida.insert(tkinter.END, txt)
+
+    @staticmethod
+    def acerca_de():
         messagebox.showinfo(message="OLC2 PROYECTO 1 \n201212535 - Mike Leonel Molina García", title="Información")
 
-    def salir(self):
+    @staticmethod
+    def salir() -> None:
         exit()
 
 root = Tk()
